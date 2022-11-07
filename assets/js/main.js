ac5b46dbe044f2e5bcc3b96029ec2308efcf48ed
Nick Khramov <mielsenmc11@gmail.com>
2022-11-07 19:32:50 +0200

    сделал, проверил

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-  $('.header-navbar__btn').on('click', function () {
+  $('.navbar__btn').on('click', function () {
     $(this).toggleClass('active');
     $('.header-navbar__list').toggleClass('active');
   });
